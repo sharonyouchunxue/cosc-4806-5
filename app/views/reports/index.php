11dbba1e18a0ef4af720d5de5ab8041140dc9bb2
sharonyouchunxue <102676646+sharonyouchunxue@users.noreply.github.com>
2024-07-05 17:45:37 +0000

    updated view report for user with most reminder

--- app/views/reports/index.php
+++ app/views/reports/index.php
@@ -8,9 +8,16 @@
 
     <h2>All Reminders</h2>
     <ul>
-        <?php foreach ($reminders as $reminder): ?>
+        <?php foreach ($data['reminders'] as $reminder): ?>
             <li><?php echo htmlspecialchars($reminder['subject']); ?> - <?php echo htmlspecialchars($reminder['reminder_time']); ?></li>
         <?php endforeach; ?>
     </ul>
+
+    <h2>User with Most Reminders</h2>
+    <?php if ($data['mostRemindersUser']): ?>
+        <p><?php echo htmlspecialchars($data['mostRemindersUser']['username']) . " with " . $data['mostRemindersUser']['reminder_count'] . " reminders"; ?></p>
+    <?php else: ?>
+        <p>No reminders found.</p>
+    <?php endif; ?>
 </body>
 </html>
