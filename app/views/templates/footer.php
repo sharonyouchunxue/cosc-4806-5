db52d7e9995e3e4817851a9dc4c896c789952b7a
sharonyouchunxue <102676646+sharonyouchunxue@users.noreply.github.com>
2024-07-08 20:42:43 +0000

    clean code

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,16 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="icon" href="/favicon.png">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <title>COSC 4806</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="mobile-web-app-capable" content="yes">
-</head>
-<body>
+
     <footer class="footer bg-light text-center text-lg-start">
         <div class="container p-4">
             <div class="row">
